fba1c9f664bc742aa55a729f326c5e4ae62e1f92
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2021-10-26 12:05:27 -0300

    adjust project and readme

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,33 +1,28 @@
 import React from 'react'
 import styles from './styles.module.css'
-import logoFb from '../../assets/fb.svg'
-import logoInstaram from '../../assets/instagram.svg'
-import logoTwitter from '../../assets/twitter.svg'
-import logoTwitch from '../../assets/twitch.svg'
-import logoYoutube from '../../assets/youtube.svg'
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
      <section className={styles.social}>
       <a href="https://twitch.com" style={{['--i' as any]: 2}} className={styles.cardSocial}>
-        <img src={logoTwitch} alt="Logo da Twitch" />
+        <img src="/assets/twitch.svg" alt="Logo da Twitch" />
       </a>
 
       <a href="https://twitch.com"  style={{['--i' as any]: 4}} className={styles.cardSocial}>
-        <img src={logoYoutube} alt="Logo do Youtube" />
+        <img src="/assets/youtube.svg" alt="Logo do Youtube" />
       </a>
 
       <a href="https://twitch.com"  style={{['--i' as any]: 6}} className={styles.cardSocial}>
-        <img src={logoTwitter} alt="Logo do Twitter" />
+        <img src="/assets/twitter.svg" alt="Logo do Twitter" />
       </a>
 
       <a href="https://twitch.com" style={{['--i' as any]: 8}} className={styles.cardSocial}>
-        <img src={logoInstaram} alt="Logo do Instagram" />
+        <img src="/assets/instagram.svg" alt="Logo do Instagram" />
       </a>
 
       <a href="https://twitch.com" style={{['--i' as any]: 10}} className={styles.cardSocial}>
-        <img src={logoFb} alt="Logo do Facebook" />
+        <img src="/assets/fb.svg" alt="Logo do Facebook" />
       </a>
     </section>
   </footer>
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from './styles.module.css'
-import gameImage from '../../assets/game.jpg'
 
 export const Main = () => {
   return (
@@ -10,7 +9,7 @@ export const Main = () => {
       <p style={{['--i' as any]: 2}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </section>
 
-    <img aria-hidden={true} style={{['--i' as any]: 2}} src={gameImage} alt="Vetor de um controle de vidiogame, com uma pessoa a direita encostado no videogame, usando um tablet e uma pessoa a esquerda sentado em uma cadeira encostada ao primeiro 'pé' do controle remoto" />
+    <img aria-hidden={true} style={{['--i' as any]: 2}} src="/assets/game.jpg" alt="Vetor de um controle de vidiogame, com uma pessoa a direita encostado no videogame, usando um tablet e uma pessoa a esquerda sentado em uma cadeira encostada ao primeiro 'pé' do controle remoto" />
   </main>
   )
 }
